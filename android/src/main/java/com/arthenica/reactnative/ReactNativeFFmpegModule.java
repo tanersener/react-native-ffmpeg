c48e0fd04d37dd94e6adc408ca71ee0851469eb1
Taner Sener <tanersener@gmail.com>
2018-07-12 09:03:38 +0300

    Android library implemented.

--- android/src/main/java/com/arthenica/reactnative/ReactNativeFFmpegModule.java
+++ android/src/main/java/com/arthenica/reactnative/ReactNativeFFmpegModule.java
@@ -1,22 +1,71 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Taner Sener
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.arthenica.reactnative;
 
+import com.arthenica.mobileffmpeg.AbiDetect;
+import com.arthenica.mobileffmpeg.FFmpeg;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class ReactNativeFFmpegModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public ReactNativeFFmpegModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public ReactNativeFFmpegModule(final ReactApplicationContext reactContext) {
+        super(reactContext);
 
-  @Override
-  public String getName() {
-    return "ReactNativeFFmpeg";
-  }
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "ReactNativeFFmpeg";
+    }
+
+    @ReactMethod
+    public static String getFFmpegVersion() {
+        return FFmpeg.getFFmpegVersion();
+    }
+
+    @ReactMethod
+    public static int execute(final String ... arguments) {
+        return FFmpeg.execute(arguments);
+    }
+
+    @ReactMethod
+    public static String getPlatform() {
+        return AbiDetect.getAbi();
+    }
+
+    /**
+     * This method is available only in Android.
+     */
+    @ReactMethod
+    public static void shutdown() {
+        FFmpeg.shutdown();
+    }
+
 }
--- android/src/main/java/com/arthenica/reactnative/ReactNativeFFmpegPackage.java
+++ android/src/main/java/com/arthenica/reactnative/ReactNativeFFmpegPackage.java
@@ -1,28 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Taner Sener
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.arthenica.reactnative;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
+
+import java.util.Collections;
+import java.util.List;
+
 public class ReactNativeFFmpegPackage implements ReactPackage {
+
     @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new ReactNativeFFmpegModule(reactContext));
+    public List<NativeModule> createNativeModules(final ReactApplicationContext reactContext) {
+        return Collections.<NativeModule>singletonList(new ReactNativeFFmpegModule(reactContext));
     }
 
-    // Deprecated from RN 0.47
+    /**
+     * Deprecated from RN 0.47
+     */
     public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+    public List<ViewManager> createViewManagers(final ReactApplicationContext reactContext) {
+        return Collections.emptyList();
     }
+
 }
